67945c29ba56e681aa63e6cf4d5ec30ad9f41b98
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-01-01 21:38:20 +0600

    added task card, added style for it, fixed the color palatte

--- todo_app/app/Components/TaskCard.jsx
+++ todo_app/app/Components/TaskCard.jsx
@@ -3,10 +3,31 @@
 import React from "react";
 import Image from "next/image";
 
-const TaskCard = ({ task, onComplete, onArchive, onDelete }) => {
-    return (
-        <div></div>
-    )
+const TaskCard = ({
+  task,
+  completedHandler,
+  archivedHandler,
+  deletedHandler,
+}) => {
+  return (
+    <div className="flex flex-col border-2 border-blue-950 rounded-lg p-3 min-w-60 bg-blue-900/20">
+      <span className="text-lg font-medium mb-2">{task.title}</span>
+      <span className="text-sm font-light mb-4 text-zinc-400">
+        {task.description}
+      </span>
+      <div className="flex gap-3 mt-auto">
+        <button className="pt-4 pr-3" onClick={() => completedHandler(task.id)}>
+          <Image src="/done.svg" height={18} width={18} alt="Completed" />
+        </button>
+        <button className="pt-4 pr-3" onClick={() => archivedHandler(task.id)}>
+          <Image src="/archive.svg" height={18} width={18} alt="Archive" />
+        </button>
+        <button className="pt-4 pr-3" onClick={() => deletedHandler(task.id)}>
+          <Image src="/delete.svg" height={18} width={18} alt="Trash" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TaskCard;
