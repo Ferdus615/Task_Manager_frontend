22029d5b08c401c77884da93701684992920bd5c
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-01-09 11:41:03 +0600

    added the new task object for archive, delete & complete finction

--- todo_app/app/Components/TaskCard.jsx
+++ todo_app/app/Components/TaskCard.jsx
@@ -5,19 +5,30 @@ import React from "react";
 
 const TaskCard = ({ title, desc }) => {
   return (
-    <div className="flex flex-col border-2 border-blue-950 rounded-lg p-3 w-60">
+    <div
+      className={`flex flex-col border-2 border-blue-950 rounded-lg p-3 w-60 ${completed} ? bg-blue-500/20 : bg-blue/200`}
+    >
       <div className="flex flex-col max-h-60 overflow-hidden">
         <span className="text-lg font-medium mb-2">{title}</span>
         <span className="text-sm font-light mb-4 text-zinc-400">{desc}</span>
       </div>
       <div className="flex pt-2 gap-7">
-        <button className="hover:cursor-pointer hover:scale-110 transition duration-200">
+        <button
+          onClick={() => onCompleted(id)}
+          className="hover:cursor-pointer hover:scale-110 transition duration-200"
+        >
           <Image src="/done.svg" width={20} height={20} alt="completed" />
         </button>
-        <button className="hover:cursor-pointer hover:scale-110 transition duration-200">
+        <button
+          onClick={() => onArchived(id)}
+          className="hover:cursor-pointer hover:scale-110 transition duration-200"
+        >
           <Image src="/archive.svg" width={22} height={20} alt="completed" />
         </button>
-        <button className="hover:cursor-pointer hover:scale-110 transition duration-200">
+        <button
+          onClick={() => onDeleted(id)}
+          className="hover:cursor-pointer hover:scale-110 transition duration-200"
+        >
           <Image src="/trash.svg" width={18} height={20} alt="completed" />
         </button>
       </div>
