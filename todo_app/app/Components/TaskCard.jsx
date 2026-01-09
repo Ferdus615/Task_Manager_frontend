a3af208f3fb1538ceb47b42751dddd79d52cb2e0
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-01-09 10:29:20 +0600

    fixed the card heigh issue

--- todo_app/app/Components/TaskCard.jsx
+++ todo_app/app/Components/TaskCard.jsx
@@ -5,10 +5,12 @@ import React from "react";
 
 const TaskCard = ({ title, desc }) => {
   return (
-    <div className="flex flex-col border-2 border-blue-950 rounded-lg p-3 w-60 max-h-50 overflow-hidden">
-      <span className="text-lg font-medium mb-2">{title}</span>
-      <span className="text-sm font-light mb-4 text-zinc-400">{desc}</span>
-      <div className="flex pt-3 gap-7">
+    <div className="flex flex-col border-2 border-blue-950 rounded-lg p-3 w-60">
+      <div className="flex flex-col max-h-60 overflow-hidden">
+        <span className="text-lg font-medium mb-2">{title}</span>
+        <span className="text-sm font-light mb-4 text-zinc-400">{desc}</span>
+      </div>
+      <div className="flex pt-2 gap-7">
         <button className="hover:cursor-pointer hover:scale-110 transition duration-200">
           <Image src="/done.svg" width={20} height={20} alt="completed" />
         </button>
--- todo_app/app/Components/TaskList.jsx
+++ todo_app/app/Components/TaskList.jsx
@@ -7,7 +7,7 @@ const TaskList = ({ tasks, emptyMsg = "No task yet! Add one above." }) => {
   return tasks.lenght === 0 ? (
     <p>{emptyMsg}</p>
   ) : (
-    <div className="flex flex-wrap gap-5 mt-10">
+    <div className="flex flex-wrap items-start gap-5 mt-10">
       {tasks.map((task) => (
         <TaskCard key={task.id} title={task.title} desc={task.desc} />
       ))}
