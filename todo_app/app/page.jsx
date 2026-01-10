ac189bfb8b2be79ad1cbaf23445878552addac97
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-01-10 15:56:59 +0600

    added the local storage storinng code

--- todo_app/app/page.jsx
+++ todo_app/app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "./Components/Sidebar";
 import Title from "./Components/Title";
 import TaskForm from "./Components/TaskForm";
@@ -7,6 +7,13 @@ import TaskList from "./Components/TaskList";
 
 function Page() {
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const storedTasks = localStorage.getItem(JSON.parse("tasks"));
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
 
   const handleAddTask = (task) => {
     setTasks((prev) => [
@@ -20,6 +27,10 @@ function Page() {
       },
     ]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <div className="flex h-screen w-screen">
