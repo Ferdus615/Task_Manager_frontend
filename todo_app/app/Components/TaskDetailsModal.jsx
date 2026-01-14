ca1d763e524a8eaece640e31c5e90fca76f88189
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2026-01-14 11:33:04 +0600

    details modal done

--- todo_app/app/Components/TaskDetailsModal.jsx
+++ todo_app/app/Components/TaskDetailsModal.jsx
@@ -1,20 +1,25 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 
 const TaskDetailsModal = ({ title, desc, onClose }) => {
   return (
     <div
       onClick={(e) => e.stopPropagation()}
-      className="absolute inset-0 m-auto w-120 h-fit p-5 rounded-lg bg-red-800 "
+      className="absolute inset-0 m-auto w-120 h-fit p-5 rounded-lg bg-gray-950"
     >
-      <div>
-        <button onClick={onClose}>X</button>
-        <h2>{title}</h2>
+      <div className="flex justify-between pb-5">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <button
+          onClick={onClose}
+          className="border p-2 rounded-full bg-blue-950 border-blue-800 hover:cursor-pointer"
+        >
+          <Image src="/crossMark.svg" width={10} height={10} alt="Close" />
+        </button>
       </div>
-      <p>{desc}</p>
+      <p className="text-sm font-light">{desc}</p>
     </div>
   );
 };
-
 export default TaskDetailsModal;
