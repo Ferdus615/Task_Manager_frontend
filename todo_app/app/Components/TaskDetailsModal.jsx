19e458b6166d0f239f91cddf790415fbe0356abf
ferdus615 <ferdus615@gmail.com>
2026-01-26 19:24:56 +0600

    old office code

--- todo_app/app/Components/TaskDetailsModal.jsx
+++ todo_app/app/Components/TaskDetailsModal.jsx
@@ -18,14 +18,14 @@ const TaskDetailsModal = ({ title, desc, onClose }) => {
       >
         <div className="flex gap-10 justify-between p-5 border-b border-[#272835]">
           <h2 className="text-xl font-bold">{title}</h2>
-          <div>
+          {/* <div>
             <button
               onClick={onClose}
               className="border p-2 rounded-full hover:cursor-pointer"
             >
               <Image src="/crossMark.svg" width={10} height={10} alt="Close" />
             </button>
-          </div>
+          </div> */}
         </div>
         <div
           className="p-5 flex-1 overflow-auto scrollbar-none 
